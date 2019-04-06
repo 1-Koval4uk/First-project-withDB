af9af1c9581625d815835386c65cf39cfad67688
Сергей Ковальчук <cergkovalcuk9@gmail.com>
2019-04-06 15:47:55 +0300

    mdb

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require("mongodb").MongoClient;
+const app = express();
 
-var app = express();
+const url = "mongodb://localhost:27017/";
+const MongoClient = new MongoClient(url, { useNewUrlParser: true });
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
+//Enter the static files
 app.use('/css', express.static('css'));
 app.use('/fonts', express.static('fonts'));
 app.use('/images', express.static('images'));
@@ -16,14 +19,47 @@ app.use('/slick', express.static('slick'));
 app.get("/", urlencodedParser, function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
-app.post("/index", urlencodedParser, function (req, res) {
-    if(!req.body) return res.sendStatus(400);
-    console.log(req.body);
-    var finend = (req.body);
-    console.log("Ниже будет переменная");
-    console.log(finend);
-    res.send(`${req.body.userText}`);
+
+app.post("/index", urlencodedParser, function (req, res){
+     if(!req.body) return res.status(400);
+     console.log(req.body);
+     var users = req.body;
+
+     db2.connect(function(err, client){
+       const db = client.db('test');
+       const collection = db.collection('users');
+            collection.insertOne(users, function(err, results){
+
+              if(err){
+                return console.log(err);
+              }
+              console.log(result.ops);
+              //res.send(users);
+              client.close()
+            });
+    console.log(users)
+      });
+    res.send(`$(req.body.userText)`);
 });
+
+res.send(`$(req.body.userText)`);
+
+
+
+
+
+
+
+
+//get the text from the form
+// app.post("/index", urlencodedParser, function (req, res) {
+//     if(!req.body) return res.sendStatus(400);
+//     console.log(req.body);
+//     var finend = (req.body);
+//     console.log("Ниже будет переменная");
+//     console.log(finend);
+//     res.send(`${req.body.userText}`);
+// });
   
 
 app.listen(3000);
